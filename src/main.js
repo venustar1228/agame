a25a98e750ebdaf71ba5b62d37605883393da92f
venustar1228 <jack-venustar1228@outlook.com>
2021-12-03 03:36:07 +0530

    init template

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Phaser from './lib/phaser.js';
+
+
+
+// console.dir(Phaser);
+
+// Phaser.LEFT
+
+
+
+export default new Phaser.Game({
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+})
